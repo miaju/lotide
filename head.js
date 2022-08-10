b3809e7b9260ad8491986e12cbe3d1d6691d3e54
Mia Tanner <mia@indygecko.com>
2022-08-10 12:40:23 -0700

    linted head.js

--- head.js
+++ head.js
@@ -12,9 +12,9 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const head = function(arr){
+const head = function(arr) {
   return arr[0];
-}
+};
 
 const arr1 = ["a","b","c"];
 const arr2 = [];
@@ -22,4 +22,4 @@ const arr3 = ["a"];
 
 assertEqual(head(arr1), "a");
 assertEqual(head(arr2), undefined);
-assertEqual(head(arr3), "a")
+assertEqual(head(arr3), "a");
