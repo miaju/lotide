b18eb6ab8708993c884c2fe52b9bead5d5292cfb
Mia Tanner <mia@indygecko.com>
2022-08-10 12:38:05 -0700

    head first implementation

---
+++ head.js
@@ -0,0 +1,25 @@
+
+const assertEqual = function(actual, expected) {
+
+  let equal = (actual === expected);
+  
+  if (equal) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+
+const head = function(arr){
+  return arr[0];
+}
+
+const arr1 = ["a","b","c"];
+const arr2 = [];
+const arr3 = ["a"];
+
+assertEqual(head(arr1), "a");
+assertEqual(head(arr2), undefined);
+assertEqual(head(arr3), "a")
