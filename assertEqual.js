b98a21fbcae16bede0afa19c830779cfe069e262
Mia Tanner <mia@indygecko.com>
2022-08-10 12:36:50 -0700

    got rid of commented out unrelevant code

--- assertEqual.js
+++ assertEqual.js
@@ -4,11 +4,11 @@ const assertEqual = function(actual, expected) {
   let equal = (actual === expected);
   
   if (equal) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
   }
-  
+
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
@@ -17,20 +17,3 @@ assertEqual("", false);
 assertEqual("Lighthouse Labs", "Lighthouse Labs");
 assertEqual(3,2);
 
-
-/*
-let testPassCount = 0;
-let testCount = 0;
- const displayResults = function(passed, testName){
-  testCount++;
-  if (passed) {
-    console.log("Passed test: " + testName);
-    testPassCount++;
-  } else {
-    console.log("Failed test: " + testName);
-  }
-  console.log();
-};
-
-
-*/
