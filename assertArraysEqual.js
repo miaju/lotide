2e9607380baabc61d0738e2d5e9cdf90411236fb
Mia Tanner <mia@indygecko.com>
2022-08-10 21:32:50 -0700

    minor tweaks to the message that prints out

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -22,9 +22,9 @@ const assertArraysEqual = function(actual, expected) {
   let equal = eqArrays(actual, expected);
 
   if (equal) {
-    console.log(`✅Assertion Passed`);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌ Assertion Failed`);
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}. If these seem to be the same, check the types of the values in the arrays.`);
   }
 
 };
