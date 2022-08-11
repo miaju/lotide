8b41e1c4b618c82085f3c51f561cc570f4aea562
Mia Tanner <mia@indygecko.com>
2022-08-10 20:51:14 -0700

    initial implementation of eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+
+  let equal = (actual === expected);
+
+  if (equal) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(arr1, arr2) {
+
+  let result = true;
+  
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  while (result) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+      }
+    }
+    break;
+  }
+  return result;
+
+};
+
+const arr2 = [];
+const arr3 = ["a"];
+const arr4 = ["This", "Is", "hard", "to", "come", "up", "with", "random", "arrays"];
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(arr3, arr4), false);
+assertEqual(eqArrays(arr2, []), true);
