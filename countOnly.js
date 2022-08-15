3883c797e217ce35ebc1ad263fb44791a3827229
Mia Tanner <mia@indygecko.com>
2022-08-15 13:12:56 -0700

    first implementation of countOnly

---
+++ countOnly.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+
+  let equal = (actual === expected);
+
+  if (equal) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  let counts = {};
+  for (const item of allItems) {
+
+    if (itemsToCount[item]) {
+      if (counts[item]) {
+        counts[item] ++;
+
+      } else {
+        counts[item] = 1;
+
+      }
+    }
+  }
+
+  return counts;
+};
+
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
