e05780be50ef0114661f55a97f5f268da263730b
Mia Tanner <mia@indygecko.com>
2022-08-30 14:27:28 -0700

    added testing for without

---
+++ test/without.test.js
@@ -0,0 +1,64 @@
+const expect = require('chai').expect;
+const without = require("../without");
+const eqArrays = require("../eqArrays");
+
+
+describe("testing without", () => {
+  let expected;
+  let actual;
+  it("should return array with one element removed for to remove array length of 1", () => {
+    expected = [2, 3];
+    actual = without([1, 2, 3], [1]);
+    expect(eqArrays(expected, actual)).to.be.true;
+  });
+  it("should just ignore elements of to the to remove array if they do not appear in the original array", () => {
+    expected = ["hummus", 1];
+    actual = without(["hummus", 1, ""], ["", "h"]);
+    expect(eqArrays(expected, actual)).to.be.true;
+  });
+  it("should remove all instances of an element of the to remove array if they appear more than once", () => {
+    expected = [1, 3];
+    actual = without([1, 2, 2, 2, 2, 6, 7, 3], [2, 7, 6]);
+    expect(eqArrays(expected, actual)).to.be.true;
+  });
+  it("should return an empty array if to remove conatins all of original array", () => {
+    expected = [];
+    actual = without([1, 2, 3], [1, 2, 3]);
+    expect(eqArrays(expected, actual)).to.be.true;
+  });
+  it("should return an empty array if given array is empty", () => {
+    expected = [];
+    actual = without([],[1, 2]);
+    expect(eqArrays(expected, actual)).to.be.true;
+  });
+});
+
+
+
+/*
+let expected;
+let actual;
+
+
+expected = [2, 3];
+actual = without([1, 2, 3], [1]);
+eqArrays(expected, actual);
+
+expected = ["1", "2"];
+actual = without(["1", "2", "3"], [1, 2, "3"]);
+eqArrays(expected, actual);
+eqArrays(actual, actual);
+
+expected = ["hummus", 1];
+actual = without(["hummus", 1, ""], ["", "h"]);
+eqArrays(expected, actual);
+
+
+expected = [1, 3];
+actual = without([1, 2, 2, 2, 2, 6, 7, 3], [2, 7, 6]);
+eqArrays(expected, actual);
+
+expected = [];
+actual = without([1], [1]);
+eqArrays(expected, actual);
+*/
--- without.js
+++ without.js
@@ -1,34 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-
-  let result = true;
-  
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  while (result) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        result = false;
-      }
-    }
-    break;
-  }
-  return result;
-
-};
-
-const assertArraysEqual = function(actual, expected) {
-
-  let equal = eqArrays(actual, expected);
-
-  if (equal) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}. If these seem to be the same, check the types of the values in the arrays.`);
-  }
-
-};
-
 const without = function(original, toRemove) {
 
   let result = [];
@@ -49,30 +18,3 @@ const without = function(original, toRemove) {
 };
 
 module.exports = without;
-
-let expected;
-let actual;
-
-
-expected = [2, 3];
-actual = without([1, 2, 3], [1]);
-assertArraysEqual(expected, actual);
-
-expected = ["1", "2"];
-actual = without(["1", "2", "3"], [1, 2, "3"]);
-assertArraysEqual(expected, actual);
-assertArraysEqual(actual, actual);
-
-expected = ["hummus", 1];
-actual = without(["hummus", 1, ""], ["", "h"]);
-assertArraysEqual(expected, actual);
-
-
-expected = [1, 3];
-actual = without([1, 2, 2, 2, 2, 6, 7, 3], [2, 7, 6]);
-assertArraysEqual(expected, actual);
-
-expected = [];
-actual = without([1], [1]);
-assertArraysEqual(expected, actual);
-
