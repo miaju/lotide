20d9faff7e0b18fe152433b5bab47d977b32c2d3
Mia Tanner <mia@indygecko.com>
2022-08-29 18:03:52 -0700

    refactored head.test to use mocha & chai

--- test/head.test.js
+++ test/head.test.js
@@ -1,11 +1,19 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const expect = require('chai').expect;
 
 const arr1 = ["a","b","c"];
 const arr2 = [];
 const arr3 = ["a"];
 
-assertEqual(head(arr1), "a");
-assertEqual(head(arr2), undefined);
-assertEqual(head(arr3), "a");
 
+describe("testing head", () => {
+  it(`should return a for ${arr1}`, () => {
+    expect(head(arr1)).to.equal("a");
+  });
+  it('should return undefined for empty array', () => {
+    expect(head(arr2)).to.be.undefined;
+  });
+  it(`should return a for ${arr3}`, () => {
+    expect(head(arr3)).to.equal("a");
+  });
+});
