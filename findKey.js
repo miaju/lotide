615cc0a1455974ef46d815beab14cad34587dfcb
Mia Tanner <mia@indygecko.com>
2022-08-30 17:11:31 -0700

    added testing for findKey

--- findKey.js
+++ findKey.js
@@ -1,16 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  let equal = (actual === expected);
-
-  if (equal) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
-
 const findKey = function(ob, cb) {
   for (let item in ob) {
     if (cb(ob[item])) {
@@ -20,34 +7,3 @@ const findKey = function(ob, cb) {
 };
 
 module.exports = findKey;
-
-const result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-
-assertEqual(result1, "noma");
-
-
-const result2 = findKey({
-  paid: true,
-  amount: 13.87,
-  cardType: 'Visa'
-}, x => x === 'Visa');
-
-assertEqual(result2, 'cardType');
-
-
-const result3 = findKey({
-  paid: true,
-  amount: 13.87,
-  cardType: 'Visa'
-}, x => x === false);
-
-assertEqual(result3, undefined);
-
-
---
+++ test/findKey.test.js
@@ -0,0 +1,40 @@
+const findKey = require("../findKey");
+const expect = require("chai").expect;
+
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+const result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+const result3 = findKey({
+  paid: true,
+  amount: 13.87,
+  cardType: 'Visa'
+}, x => x === false);
+
+
+describe("testing findKey", () => {
+  it("should return the key of whose value returns a truthy value from the callback function", () => {
+    expect(result1).to.equal("noma");
+  });
+  it('should return undefined if callback always gives falsy', () => {
+    expect(result3).to.be.undefined;
+  });
+  it("should return the first truthy value if more than one will return true", () => {
+    expect(result2).to.equal("Akaleri");
+  });
+});
+
